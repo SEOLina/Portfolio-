5ab3a8fa8f6de0dea16a6dbfef625481b19791ea
SEOLina <138318964+SEOLina@users.noreply.github.com>
2023-07-19 20:35:56 +0500

    Update buttonscript.js

--- buttonscript.js
+++ buttonscript.js
@@ -3,14 +3,22 @@ function toggleVersion() {
   const container = document.getElementById('container');
   const versionButton = document.getElementById('versionButton');
   
-  body.classList.toggle('mobile-version');
-  body.classList.toggle('desktop-version');
-  container.classList.toggle('mobile-version');
-  container.classList.toggle('desktop-version');
-  
-  if (body.classList.contains('mobile-version')) {
+  if (body.classList.contains('desktop-version')) {
+    body.classList.remove('desktop-version');
+    body.classList.add('mobile-version');
+    container.classList.remove('desktop-version');
+    container.classList.add('mobile-version');
     versionButton.textContent = 'Switch to Desktop Version';
   } else {
+    body.classList.remove('mobile-version');
+    body.classList.add('desktop-version');
+    container.classList.remove('mobile-version');
+    container.classList.add('desktop-version');
     versionButton.textContent = 'Switch to Mobile Version';
   }
 }
+
+// Run toggleVersion() initially to set the correct version on page load
+toggleVersion();
+
+versionButton.addEventListener('click', toggleVersion);
